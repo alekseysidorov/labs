f67bac1e6dcba4541ca5bacc16a441de39753922
Aleksey <gorthauer87@yandex.ru>
2016-01-20 13:37:18 +0300

    Make first lab

--- olga/editor/main.cpp
+++ olga/editor/main.cpp
@@ -1,7 +1,18 @@
 #include <QApplication>
+#include <QDir>
 #include "mainwindow.h"
+#include "container.h"
 
 int main(int argc, char **argv) {
+    QString dd = QDir::home().absoluteFilePath("Figures/test");
+    Container f;
+    f.addFigure(new Circle("b", "black", 35));
+    f.addFigure(new Triangle("t", "magenta", QPoint(), QPoint()));
+    f.addFigure(new Rect("rar", "red", 100, 25));
+    f.figureAt(2)->setPos(QPoint(15, 35));
+    f.figureAt(0)->setRotation(45);
+    f.save(dd);
+
     QApplication app(argc, argv);
 
     MainWindow w;
