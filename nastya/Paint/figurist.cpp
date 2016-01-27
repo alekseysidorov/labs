016a1292da85086b0e906f3701d5768caa9a1ac0
Aleksey Sidorov <alexey.sidorov@opensoftdev.ru>
2016-01-27 15:56:06 +0300

    Figurist

--- nastya/Paint/figurist.cpp
+++ nastya/Paint/figurist.cpp
@@ -11,26 +11,45 @@ int main(int argc, char *argv[])
         FigLibrary figa;
         figa.load(pp);
 
+        std::cout << "Figs" << std::endl;
         for (int i = 0; i < figa.figss().size(); ++i) {
             Figure *f = figa.figss()[i];
-            std::cout << f->nam().toStdString() << "\n"
+            std::cout << "\ntyp: " << f->typ().toStdString() << "\n"
+                      << "nam:" << f->nam().toStdString() << "\n"
                       << "x: " << f->x() << "\n"
                       << "y: " << f->y() << "\n"
                       << "angl: " << f->angl() << "\n"
-                      << "col: " << f->col().toStdString() << "\n";
+                      << "col: " << f->col().toStdString() << std::endl;
         }
     }else if (s == "s"){
+        FigLibrary figa;
         int n;
         std::cin >> n;
         for (int i = 0; i < n; ++i) {
             std::cin >> s;
             if (s == "circl") {
-
+                int x, y, d;
+                std::string nam, cc;
+                std::cin >> nam >> cc >> x >> y >> d;
+                Circle *c = new Circle(x, y, d, QString::fromStdString(cc), QString::fromStdString(nam));
+                figa.add(c);
             }else if(s == "rect"){
-
+                int x, y, w,h;
+                double a;
+                std::string nam, cc;
+                std::cin >> nam >> cc >> x >> y >> w >> h >> a;
+                Rectanglee *r = new Rectanglee(x, y, w, h, QString::fromStdString(cc), QString::fromStdString(nam));
+                r->setAngl(a);
+                figa.add(r);
             }else if(s == "star"){
-
+                int x, y, r; double a;
+                std::string nam, cc;
+                std::cin >> nam >> cc >> x >> y >> r >> a;
+                Star *sss = new Star(x, y,QString::fromStdString(cc), QString::fromStdString(nam), r);
+                sss->setAngl(a);
+                figa.add(sss);
             }
         }
+        figa.save(pp);
     }
 }
