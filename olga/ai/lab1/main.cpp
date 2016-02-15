92bfdd5bcde8e3d49224a70b76c337638afb80b6
Aleksey Sidorov <alexey.sidorov@opensoftdev.ru>
2016-02-15 13:15:51 +0300

    Linear gradient function

--- olga/ai/lab1/main.cpp
+++ olga/ai/lab1/main.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 
 // некоторые константы
-const double eps = 0.001; // заданная точность
+const double eps = 0.1; // заданная точность
 const double h = 0.000001; // приращение аргумента
 
 // тип исследуемой функции (возвращает double, принимает два аргумента типа double)
@@ -20,8 +20,8 @@ double d_x(my_function f, double x, double y)
 // програмно считаем производную по y
 double d_y(my_function f, double x, double y)
 {
-    double a1 = f(x, y + h * 0.5);
-    double a2 = f(x, y - h * 0.5);
+    double a1 = f(x, y - h * 0.5);
+    double a2 = f(x, y + h * 0.5);
     return (a2 - a1) / h;
 }
 
@@ -29,12 +29,43 @@ struct point
 {
     double x;
     double y;
+
+    point(double x_, double y_) : x(x_), y(y_) {}
 };
 
-// простой градиентный спуск
-point gradient1(my_function, double x, double y)
+double length(double x, double y)
 {
+    return std::sqrt(x * x + y * y);
+}
 
+// простой градиентный спуск
+point gradient1(my_function f, point p, point to, int iters, double d)
+{
+    point p0 = p;
+    for (int i = 0; i < iters; ++i) {
+        double dx = d_x(f, p.x, p.y);
+        double dy = d_y(f, p.x, p.y);
+        double n = length(p.x, p.y);
+
+        p.x -= d * (dx / n);
+        p.y -= d * (dy / n);
+
+        // вычисляем расстояние до истинного минимума
+        dx = std::abs(p.x - to.x);
+        dy = std::abs(p.y - to.y);
+        double l = length(dx, dy);
+
+        std::cout << i << ";" << p.x << ";" << p.y << ";" << l << std::endl;
+
+        // вычисляем дельту между предыдущим и этим шагом и выходим если она становится меньше eps
+        dx = std::abs(p.x - p0.x);
+        dy = std::abs(p.y - p0.y);
+        if (length(dx, dy) < eps * d) {
+            break;
+        }
+        p0 = p;
+    }
+    return p;
 }
 
 // набор исследуемых функий
@@ -45,8 +76,5 @@ double func1(double x, double y)
 
 int main(int, char **)
 {
-    double dx = d_x(func1, 1, 0);
-    double dy = d_y(func1, 0, 2);
-
-    std::cout << dx << " " << dy << std::endl;
+    point p = gradient1(func1, point(27, 31), point(0, 1), 10000, 0.1);
 }
