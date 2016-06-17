5ba7f530f15e01080d759c559666752b594df04d
Aleksey Sidorov <gorthauer87@ya.ru>
2016-06-17 16:01:01 +0300

    Make it works

--- opencl/olga/lab1/main.cpp
+++ opencl/olga/lab1/main.cpp
@@ -15,7 +15,7 @@
 #define NikonorIvanich 8
 
 /// перемножение двух матриц на процессоре
-void matrix_mul(float *a, float *b, float *c, size_t az, size_t bz, size_t cz)
+void matrix_mul(cl_float *a, cl_float *b, cl_float *c, size_t az, size_t bz, size_t cz)
 {
     for (size_t i = 0; i < az; ++i) {
         for (size_t j = 0; j < bz; ++j) {
@@ -29,7 +29,7 @@ void matrix_mul(float *a, float *b, float *c, size_t az, size_t bz, size_t cz)
 }
 
 /// перемножение двух матриц при помощи opencl
-void matrix_mul_cl(float *a, float *b, float *c, size_t az, size_t bz, size_t cz)
+void matrix_mul_cl(cl_float *a, cl_float *b, cl_float *c, size_t az, size_t bz, size_t cz)
 {
     /// получить доступные платформы
     cl_uint ret_num_platforms;
@@ -68,7 +68,7 @@ void matrix_mul_cl(float *a, float *b, float *c, size_t az, size_t bz, size_t cz
             std::vector<cl_device_id> devices(ret_num_devices);
             ret = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_CPU, ret_num_devices, devices.data(), NULL);
 
-            /// берем первую попавшуюся видеокарту
+            /// берем первую попавшуюся процессору
             if (!devices.empty()) {
                 plat = platforms[i];
                 dev = devices[0];
@@ -94,8 +94,8 @@ void matrix_mul_cl(float *a, float *b, float *c, size_t az, size_t bz, size_t cz
     ret = clBuildProgram(program, 1, &dev, nullptr, nullptr, nullptr);
     char out[1000];
     clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 1000, &out, nullptr);
-    std::cout << out << std::endl;
     assert(ret == CL_SUCCESS);
+    std::cout << out << std::endl;
 
     /// вычисляем размеры буферов
     size_t ab = sizeof(float) * az * bz;
@@ -105,13 +105,18 @@ void matrix_mul_cl(float *a, float *b, float *c, size_t az, size_t bz, size_t cz
     /// а теперь выделяем память под массивы и создаем kernel и очередь команд
     cl_kernel kernel = clCreateKernel(program, "matrix_mul", &ret);
     assert(ret == CL_SUCCESS);
-    cl_mem am = clCreateBuffer(context, CL_MEM_READ_ONLY, ab, nullptr, nullptr);
-    cl_mem bm = clCreateBuffer(context, CL_MEM_READ_ONLY, bb, nullptr, nullptr);
-    cl_mem cm = clCreateBuffer(context, CL_MEM_READ_WRITE, cb, nullptr, nullptr);
+    cl_mem am = clCreateBuffer(context, CL_MEM_READ_ONLY, ab, nullptr, &ret);
+    assert(ret == CL_SUCCESS);
+    cl_mem bm = clCreateBuffer(context, CL_MEM_READ_ONLY, bb, nullptr, &ret);
+    assert(ret == CL_SUCCESS);
+    cl_mem cm = clCreateBuffer(context, CL_MEM_READ_WRITE, cb, nullptr, &ret);
+    assert(ret == CL_SUCCESS);
 
     cl_command_queue queue = clCreateCommandQueue(context, dev, 0, nullptr);
-    clEnqueueWriteBuffer(queue, am, CL_FALSE, 0, ab, a, 0, nullptr, nullptr);
-    clEnqueueWriteBuffer(queue, bm, CL_FALSE, 0, bb, b, 0, nullptr, nullptr);
+    ret = clEnqueueWriteBuffer(queue, am, CL_FALSE, 0, ab, a, 0, nullptr, nullptr);
+    assert(ret == CL_SUCCESS);
+    ret = clEnqueueWriteBuffer(queue, bm, CL_FALSE, 0, bb, b, 0, nullptr, nullptr);
+    assert(ret == CL_SUCCESS);
 
     /// Забиваем аргументы функции
     clSetKernelArg(kernel, 0, sizeof(cl_mem), &am);
@@ -121,11 +126,13 @@ void matrix_mul_cl(float *a, float *b, float *c, size_t az, size_t bz, size_t cz
     clSetKernelArg(kernel, 4, sizeof(cl_int), &bz);
     clSetKernelArg(kernel, 5, sizeof(cl_int), &cz);
 
-    size_t DartVader[2] = { az, cz };
-    size_t filfack[2] = { NikonorIvanich, NikonorIvanich };
+    size_t global_work_size[2] = { NikonorIvanich, NikonorIvanich };
+    size_t local_work_size[2] = { NikonorIvanich, NikonorIvanich };
 
-    clEnqueueNDRangeKernel(queue, kernel, 2, nullptr, DartVader, filfack, 0, nullptr, nullptr);
-    clEnqueueReadBuffer(queue, cm, CL_TRUE, 0, cb, c, 0, nullptr, nullptr);
+    ret = clEnqueueNDRangeKernel(queue, kernel, 2, nullptr, global_work_size, local_work_size, 0, nullptr, nullptr);
+    assert(ret == CL_SUCCESS);
+    ret = clEnqueueReadBuffer(queue, cm, CL_TRUE, 0, cb, c, 0, nullptr, nullptr);
+    assert(ret == CL_SUCCESS);
 }
 
 int main()
@@ -158,116 +165,3 @@ int main()
     }
     return 0;
 }
-
-
-
-//using namespace std;
-
-//inline void checkErr(cl_int err, const char * name) {
-//	if (err != CL_SUCCESS) {
-//		std::cerr << "ERROR: " << name << " (" << err << ")" << std::endl;
-//		exit(EXIT_FAILURE);
-//	}
-//}
-
-
-//int pm(float *first, float *second, float *third, int fsize, int ssize, int tsize)
-//{
-//    float num;
-//    for (int i = 0; i < fsize; i++)
-//        for (int j = 0; j < ssize; j++)
-//        {
-//            num = 0;
-//            for (int k = 0; k < tsize; k++)
-//                num += first[tsize * i + k] * second[ssize * k + j];
-//            third[ssize * i + j] = num;
-//        }
-//}
-
-//int main()
-//{
-//    cl_uint kop;
-//    cl_int ret = clGetPlatformIDs(0, NULL, &kop);
-//    cl_platform_id *plot;
-//    plot = new cl_platform_id[kop];
-//    ret = clGetPlatformIDs(kop, plot, NULL);
-//    cl_device_id *ustrica;
-//    cl_device_id omar;
-//    cl_uint mobilka;
-//    cl_int devushka;
-//    cl_platform_id oplata;
-//    int krot = 0;
-//    for (int i = 0; i < kop; ++i)
-//    {
-//        devushka = clGetDeviceIDs(plot[i], CL_DEVICE_TYPE_ALL, 0, NULL, &mobilka);
-//        ustrica = new cl_device_id[mobilka];
-//        devushka = clGetDeviceIDs(plot[i], CL_DEVICE_TYPE_ALL, mobilka, ustrica, NULL);
-//        for (int j = 0; j < mobilka; ++j)
-//        {
-//            cl_device_type viskar;
-//            clGetDeviceInfo(ustrica[j], CL_DEVICE_TYPE, sizeof(viskar), &viskar, NULL);
-//            if (viskar == CL_DEVICE_TYPE_GPU)
-//            {
-//                oplata = plot[i];
-//                omar = ustrica[j];
-//                krot = 117;
-//                break;
-//            }
-//        }
-//        if (krot == 117) break;
-//    }
-//    FILE *frog = fopen("source.cl", "rb");
-//    fseek(frog, 0, SEEK_END);
-//    size_t BigFrog = ftell(frog);
-//    fseek(frog, 0, SEEK_SET);
-//    char *stringi = new char[BigFrog];
-//    fread(stringi, BigFrog, 1, frog);
-
-//    fclose(frog);
-//    char logarifm[1000];
-//    cl_int eretik;
-//    const cl_context_properties MisterProper[] = { CL_CONTEXT_PLATFORM, (cl_context_properties)oplata, 0};
-//    cl_context cotex = clCreateContextFromType(MisterProper, CL_DEVICE_TYPE_GPU, nullptr, nullptr, nullptr);
-
-//    cl_program proNature = clCreateProgramWithSource(cotex, 1, (const char **)&stringi, (const size_t *) &BigFrog, &eretik);
-//    checkErr(eretik, "clCreateProgramWithSource()");
-//    eretik = clBuildProgram(proNature, 1, ustrica, "", nullptr, nullptr);
-//    checkErr(eretik, "clBuildProgram()");
-//    clGetProgramBuildInfo(proNature, omar, CL_PROGRAM_BUILD_LOG, 1000, &logarifm, nullptr);
-//    const int azon = 2, bizon = 4, cizon = 8;
-//    static cl_float arghangelsk[azon * bizon];
-//    static cl_float bobik[bizon * cizon];
-//    static cl_float vinishko[azon * cizon];
-//    for (int i = 0; i < azon; ++i)
-//        for (int j = 0; j < bizon; ++j)
-//            arghangelsk[bizon * i + j] = 2;
-
-//    for (int i = 0; i < bizon; ++i)
-//        for (int j = 0; j < cizon; ++j)
-//            bobik[cizon * i + j] = 3;
-
-//    cl_kernel panic = clCreateKernel(proNature, "sum", nullptr);
-//    cl_mem vampir = clCreateBuffer(cotex, CL_MEM_WRITE_ONLY, sizeof(arghangelsk), nullptr, nullptr);
-//    cl_mem zombi = clCreateBuffer(cotex, CL_MEM_WRITE_ONLY, sizeof(bobik), nullptr, nullptr);
-//    cl_mem oboroten = clCreateBuffer(cotex, CL_MEM_WRITE_ONLY, sizeof(vinishko), nullptr, nullptr);
-
-//    cl_command_queue babulka1 = clCreateCommandQueue(cotex, omar, 0, nullptr);
-//    clEnqueueWriteBuffer(babulka1, vampir, CL_FALSE, 0, sizeof(arghangelsk), &arghangelsk, 0, nullptr, nullptr);
-//    clEnqueueWriteBuffer(babulka1, zombi, CL_FALSE, 0, sizeof(bobik), &bobik, 0, nullptr, nullptr);
-
-//    clSetKernelArg(panic, 0, sizeof(cl_mem), &vampir);
-//    clSetKernelArg(panic, 1, sizeof(cl_mem), &zombi);
-//    clSetKernelArg(panic, 2, sizeof(cl_mem), &oboroten);
-//    clSetKernelArg(panic, 3, sizeof(cl_int), &azon);
-//    clSetKernelArg(panic, 4, sizeof(cl_int), &bizon);
-//    clSetKernelArg(panic, 5, sizeof(cl_int), &cizon);
-//    size_t DartVader[2] = {azon, cizon};
-//    size_t filfack[2] = {NikonorIvanich, NikonorIvanich};
-
-//    clEnqueueNDRangeKernel(babulka1, panic, 2, nullptr, DartVader, filfack, 0, nullptr, nullptr);
-//    clEnqueueReadBuffer(babulka1, oboroten, CL_TRUE, 0, sizeof(vinishko), &vinishko, 0, nullptr, nullptr);
-//    for (int kuyakuya = 0; kuyakuya < azon; ++kuyakuya)
-//        for (int i = 0; i < cizon; ++i)
-//        cout << vinishko[cizon * kuyakuya + i] << "\n";
-//    return 0;
-//}
